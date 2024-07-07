89aa32183d5a0892d52d093b3ad23c567b66d19b
manas-shinde <manas.a.shinde11@gmail.com>
2024-07-07 11:52:11 +0530

    feat : skip button implementation.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Define variable
 const player = document.querySelector(".player");
 const video = document.querySelector(".viewer");
 const progress = document.querySelector(".progress");
@@ -6,13 +7,25 @@ const toggle = document.querySelector(".toggle");
 const skipButtons = document.querySelectorAll("[data-skip]");
 const range = document.querySelectorAll(".player__slider");
 
+// Build necessary Functions
 function togglePlay() {
   video[video.paused ? "play" : "pause"]();
 }
+
 function updatePlayButton() {
   toggle.textContent = this.paused ? "►" : "❚ ❚";
 }
+
+function skip() {
+  console.log(`Skip the video to ${this.dataset.skip}`);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Hook up the event Listeners
 video.addEventListener("click", togglePlay);
-video.addEventListener("play", updatePlayButton);
-video.addEventListener("pause", updatePlayButton);
 toggle.addEventListener("click", togglePlay);
+
+video.addEventListener("pause", updatePlayButton);
+video.addEventListener("play", updatePlayButton);
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
