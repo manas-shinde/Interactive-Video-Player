1a0a460d5d8f688daa5dd6ccbd309a19722580b8
manas-shinde <manas.a.shinde11@gmail.com>
2024-07-07 12:33:18 +0530

    feat : added support for playback speed and progress bar.

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const progress = document.querySelector(".progress");
 const progressBar = document.querySelector(".progress__filled");
 const toggle = document.querySelector(".toggle");
 const skipButtons = document.querySelectorAll("[data-skip]");
-const range = document.querySelectorAll(".player__slider");
+const ranges = document.querySelectorAll(".player__slider");
 
 // Build necessary Functions
 function togglePlay() {
@@ -20,7 +20,19 @@ function skip() {
   console.log(`Skip the video to ${this.dataset.skip}`);
   video.currentTime += parseFloat(this.dataset.skip);
 }
+function handleRangeChange() {
+  video[this.name] = this.value;
+}
 
+function handleProgress() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercentage}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
 // Hook up the event Listeners
 video.addEventListener("click", togglePlay);
 toggle.addEventListener("click", togglePlay);
@@ -28,4 +40,14 @@ toggle.addEventListener("click", togglePlay);
 video.addEventListener("pause", updatePlayButton);
 video.addEventListener("play", updatePlayButton);
 
+//handle progress bar
+video.addEventListener("timeupdate", handleProgress);
+
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeChange));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeChange)
+);
+
+progress.addEventListener("click", scrub);
